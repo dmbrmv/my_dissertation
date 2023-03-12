fd81e764f442db7e239b235ed9ccbafe9c2d90d1
dmbrmv <dmbrmv96@gmail.com>
2023-03-12 17:00:21 +0000

    Further development of rnn model

--- input_creator/scripts/readers.py
+++ input_creator/scripts/readers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import xarray as xr
 
 
 def read_with_date_index(file_path: str):
@@ -8,3 +9,10 @@ def read_with_date_index(file_path: str):
     data = data.set_index('date')
     
     return data
+
+
+def xr_opener(file):
+    ds = xr.open_dataset(file)
+    if 'index' in ds.coords:
+        ds = ds.rename({'index': 'date'}) 
+    return ds
