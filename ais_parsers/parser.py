bd428ab2e6923cce09297b6fcee816508fb41899
dmbrmv <dmbrmv96@gmail.com>
2023-05-11 14:33:51 +0000

    Reworked parser with html reader

--- ais_parsers/parser.py
+++ ais_parsers/parser.py
@@ -1,34 +1,33 @@
 import glob
 from pathlib import Path
 from tqdm import tqdm
-from scripts.ais_parsers import level_to_csv, discharge_to_csv
-import pandas as pd
+from scripts.ais_parsers import discharge_to_csv
 
-h_path = glob.glob('../geo_data/ais_gmvo_16_03_23/levels/*.xlsx')
+# h_path = glob.glob('../geo_data/ais_gmvo_16_03_23/levels/*.xlsx')
 
-id_lbl_asso = list()
-for f_path in tqdm(h_path):
-    h_name = f_path.split('/')[-1][:-5]
-    id_lbl_asso.append(level_to_csv(data_path=f_path,
-                                    from_top=39,
-                                    save_path=Path(
-                                        f'./data/res_levels/{h_name}')))
+# id_lbl_asso = list()
+# for f_path in tqdm(h_path):
+#     h_name = f_path.split('/')[-1][:-5]
+#     id_lbl_asso.append(level_to_csv(data_path=f_path,
+#                                     from_top=39,
+#                                     save_path=Path(
+#                                         f'./data/res_levels/{h_name}')))
 
-temp_df = list()
-for record in id_lbl_asso:
-    temp_df.append(pd.DataFrame(record).T)
-temp_df = pd.concat(temp_df)
-temp_df = temp_df.rename(columns={0: 'name',
-                                  1: 'height'})
-temp_df.to_csv('./data/height_id.csv')
+# temp_df = list()
+# for record in id_lbl_asso:
+#     temp_df.append(pd.DataFrame(record).T)
+# temp_df = pd.concat(temp_df)
+# temp_df = temp_df.rename(columns={0: 'name',
+#                                   1: 'height'})
+# temp_df.to_csv('./data/height_id.csv')
 
 
-q_path = glob.glob('../geo_data/ais_gmvo_16_03_23/discharge/*.xlsx')
+q_path = glob.glob('../geo_data/ais_discharge_11_05_2023/*.xls')
 
 q_lbl_asso = list()
 for i, f_path in enumerate(tqdm(q_path)):
     print(f'{f_path}, -- {i+1}')
-    q_name = f_path.split('/')[-1][:-5]
+    q_name = f_path.split('/')[-1][:-4]
     q_lbl_asso.append(
         discharge_to_csv(data_path=f_path,
                          save_path=Path(
